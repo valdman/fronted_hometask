ac3c03f7c6ce93c9934b5ae5d5959eeb1177b7a6
ierehon1905 <you@example.com>
2018-09-21 17:35:03 +0300

    Some changes

--- first/leon/src/lodtask.js
+++ first/leon/src/lodtask.js
@@ -1,81 +1,59 @@
 import fetch from 'node-fetch';
 
 export const lodtask = () => {
-    //main array
     const ans = [];
+    const baseLink = "https://fund.mipt.ru/api/pages";
     
-    const requestApi = (page) => {
-        //Main request to "tree"
-        return fetch(`https://fund.mipt.ru/api/pages/${page}`)
+    function requestTree(page) {
+        return fetch(`${baseLink}/${page}`)
         .then((res) => res.json())
-        .then(res => {
-            //Organizing/Assembling main skeleton flat array
-             seekInfo(res);
-             return ans
-            }
-        )
-        .then(arr => {
-            //preparing list of f() that return promises 
-            const requests = arr.map((value) => sendReq(value.url, ans.indexOf(value)));
-            
-            return sendAll(requests);
-            console.log("i should not be here");
-        })
-        .then(() => ans)
         .catch(err => {
-            //В принципе сюда ни одна ошибка не должна добраться, тк все хендлится внутри 
-            console.log("Yet another broken page: ", err);
-            return "Something is bad in requastApi catch";//Promise.resolve([]);
+            console.log(err);
         })
     };
 
     function seekInfo(obj, parentStr = "") {
-        //gathering info in tree
-        /*
-        Тут можно по-разномы добывать url. Либо так как я сделал,
-         либо после в эпизоде с конкретным запросом на сервер с id (тк API может отвечать тупо по номеру стр).
-          Так мне кажется даже легче.
-        */
-        let info = {
+        const info = {
             id: obj.id,
             name: obj.pageName,
-            url: `https://fund.mipt.ru/api/pages${parentStr.length>0 ? '/' + parentStr : ''}/${obj.pageName}`,
+            url: `${baseLink}${parentStr.length>0 ? '/' + parentStr : ''}/${obj.pageName}`,
         };
         ans.push(info);
-        //function will seek new objects untill nodes are empty
-        if(obj.nodes.length > 0) {
-            for(let i = 0; i<obj.nodes.length; i++){
-                seekInfo(obj.nodes[i], parentStr.length>0 ?  parentStr+"/"+ obj.pageName: obj.pageName);
-            }
-        }
+        obj.nodes.map((elt) => {seekInfo(elt, parentStr.length>0 ?  parentStr+"/"+ obj.pageName: obj.pageName)})
+     
     }
 
     function sendAll(req) {
         return Promise.all(req)
-        //then and catch for debug 
-            .then(res => {
+            .then(() => {
                 return "ok"
             })
-            .catch(() => {
-                return "Somthing is bad in send all catch"
-            });
     }
 
     function sendReq(url, index) {
-        //console.log(url);
         return fetch(url)
         .then(res => res.json())
-        .then(({creatingTime, description}) => {
-            /*
-              You can take URL from the deconstructor actually)
-            */
-            ans[index].creatingTime = creatingTime;
-            ans[index].description = description;
+        .then(res => {
+            pushInfo(res, index);
         })
         .catch((err) => {
             console.log("Unable to fetch or JSONify page with url: ", url)
         });
     }
-    
-    return requestApi("tree");
+
+    function pushInfo({creatingTime, description, url}, index) {
+         ans[index].creatingTime = creatingTime;
+         ans[index].description = description;
+         ans[index].url2 = `${baseLink}/${url?url:''}`;
+    }
+
+    return requestTree("tree")
+            .then(res => {
+                seekInfo(res);
+                return ans
+            })
+            .then(arr => {
+                return sendAll(arr.map((value, id) => sendReq(value.url, id)));
+            })
+            .then(() => ans)
 }
