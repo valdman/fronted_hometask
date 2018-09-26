7da740e9c2d89a77d92ff40bcef0e30f60809612
Boris Valdman <boris.valdman@live.ru>
2018-09-27 00:28:16 +0300

    server for secon hometask added

---
+++ second/server/src/users.js
@@ -0,0 +1,36 @@
+import {readFileSync} from "fs"
+
+import {guid, doAndRepeat} from "./utils";
+
+class UsersRepo {
+
+    constructor(){
+        doAndRepeat(this.resetUsers, 10 * 60 * 1000)
+    }
+
+    exists = (login) => {
+        const found = this.users.filter((u) => u.login === login)
+        return found.length !== 0 && found[0].token;
+    }
+
+    create = (login) => {
+        const id = guid();
+        this.users.push({login, token: id})
+        return id;
+    }
+
+    resetUsers = () => {
+        console.log("Users list reseted");
+        const data = JSON.parse(readFileSync("data.json"));
+        this.users = data;
+    }
+
+    takeItem = (token, itemId)  => {
+        const i = this.users.findIndex((u) => u.token === token);
+        if(i === -1 ) return i;
+        this.users[i].items = !this.users[i].items ? [ itemId ] : [...this.users[i].items, itemId];
+        return itemId;
+    }
+}
+
+export default new UsersRepo();
