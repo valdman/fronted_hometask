e4c65a78fd3c9f4b0f8752a87304a879b8f963dd
ierehon1905 <you@example.com>
2018-10-07 17:06:00 +0300

    1Task completion

--- third/src/components/Task/Task.js
+++ third/src/components/Task/Task.js
@@ -9,20 +9,25 @@ class Task extends Component {
     constructor(props) {
         super(props);
         this.task = props.task
-        this.state = {
-            done: false
-        };
+        // this.state = {
+        //     done: false
+        // };
+
     };
-    
+
+    state = {
+        done: false
+    }
+
     ChangeTaskState = () => {
-        this.setState((state, props) => {
-            return {done: state.done ? false : true }
-        });
+        this.setState(
+            { done: this.state.done ? false : true }
+        );
         //{ done: this.state.done ? false : true }
     };
-    
+
     render() {
-        return <div className="Task">
+        return <div className={`Task ${this.state.done ? "done" : ""} `}>
             <TaskCheckbox onchange={this.ChangeTaskState.bind(this)} checked={this.state.done} key={"TaskCheckbox" + this.task}>
             </TaskCheckbox>
             <TaskText task={this.task} striked={this.state.done} key={"TaskText" + this.task}>
--- third/src/components/TaskCheckBox/TaskCheckBox.js
+++ third/src/components/TaskCheckBox/TaskCheckBox.js
@@ -1,22 +1,34 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./TaskCheckbox.css"
 
-class TaskCheckbox extends Component
-{
-    constructor(props)
-    {
+class TaskCheckbox extends Component {
+    constructor(props) {
         super(props);
         //this.onchange = props.onchange;
-        onchange = props.onchange;
+        //lol = props.onchange;
+        this.id = Math.floor(Math.random() * 10000);
     }
-
     // state = {
     //     checked: false
     // }
 
-    render()
-    {
-        return <input type="checkbox" className={`TaskCheckbox ${this.props.checked ? "checked": ""}`} />
+    // render() {
+    //     return <input type="checkbox"
+    //         onChange={this.props.onchange}
+    //         className={`TaskCheckbox ${this.props.checked ? "checked" : ""}`}
+    //         checked={this.props.checked}
+    //     />
+    // }
+
+    render() {
+        var newClass = this.props.checked ? "checked" : "";
+        return <div className="checkboxContainer">
+            <input className={`inp-cbx ${newClass}`} id={this.id} type="checkbox" onChange={this.props.onchange}/>
+            <label className={`cbx ${newClass}`} for={this.id}><span>
+                <svg width="14px" height="14px" viewbox="0 0 14 14">
+                    <polyline points="1.5 8 5.5 12 12.5 1.5"></polyline>
+                </svg></span></label>
+        </div>
     }
 }
 
